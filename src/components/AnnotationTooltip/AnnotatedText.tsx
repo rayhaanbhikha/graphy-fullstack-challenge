265d15039270e13cb23806b2cb4f2ea467db7dae
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2021-01-26 14:23:15 +0000

    autofocus implemented on AnnotatedText component.

--- src/components/AnnotationTooltip/AnnotatedText.tsx
+++ src/components/AnnotationTooltip/AnnotatedText.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect, useRef } from 'react'
 
 interface IAnnotatedText {
   isEditState: boolean;
@@ -6,15 +6,24 @@ interface IAnnotatedText {
   onChangeHandler: (event: React.ChangeEvent) => void
 }
 
-export const AnnotatedText: FunctionComponent<IAnnotatedText> = ({ isEditState, onChangeHandler, text }) =>
-  <textarea onChange={onChangeHandler} value={text} readOnly={!isEditState}
-    style={{
-      flexGrow: 3,
-      minWidth: 'inherit',
-      border: 'none',
-      background: isEditState ? 'white' : 'inherit',
-      margin: '5px',
-      padding: '2px',
-    }
-    }
-  />
+export const AnnotatedText: FunctionComponent<IAnnotatedText> = ({ isEditState, onChangeHandler, text }) => {
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+
+  useEffect(() => {
+    if (isEditState) textAreaRef?.current?.focus();
+  }, [isEditState])
+
+  return (
+    <textarea ref={textAreaRef as any} onChange={onChangeHandler} value={text} readOnly={!isEditState}
+      style={{
+        flexGrow: 3,
+        minWidth: 'inherit',
+        border: 'none',
+        background: isEditState ? 'white' : 'inherit',
+        margin: '5px',
+        padding: '2px',
+      }
+      }
+    />
+  )
+}
