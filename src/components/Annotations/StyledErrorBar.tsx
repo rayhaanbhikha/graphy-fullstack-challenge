cfa40ed13121e305bb207f54da714c3214b7db39
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2021-01-28 14:46:01 +0000

    style error bar fixed.

--- src/components/Annotations/StyledErrorBar.tsx
+++ src/components/Annotations/StyledErrorBar.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
 export const StyledErrorBar = styled.div`
-  background: #d8d8d838;
+  background: #f9f9f9;
   max-width: 300px;
   text-align: center;
   color: #ff4b4b;
   padding: 5px;
   border-radius: 5px;
+  z-index: 1000;
+  position: fixed;
 `
