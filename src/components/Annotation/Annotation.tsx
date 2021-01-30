29fede2746419130e02f102db94314b4901b7b3b
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2021-01-30 19:06:28 +0000

    Now working properly.

--- src/components/Annotation/Annotation.tsx
+++ src/components/Annotation/Annotation.tsx
@@ -1,11 +1,9 @@
-import React, { FunctionComponent, useEffect, useReducer, useState } from 'react'
+import React, { FunctionComponent, useEffect, useState } from 'react'
 
 import { StyledMarker } from '../Marker/StyledMarker';
 import { AnnotationWithStateType } from '../../types';
 import { AnnotationTooltip } from '../AnnotationTooltip/AnnotationTooltip';
 import { ApplicationState } from '../Annotations/Annotations';
-import { AnnotationActions, annotationStateReducer } from './annotationReducer';
-import { useAnnotations } from '../hooks/useAnnotations';
 
 export interface IAnnotation {
   data: AnnotationWithStateType;
@@ -22,98 +20,59 @@ export enum AnnotationStates {
   DRAGGING = "DRAGGING"
 }
 
-const useAnnotation = (initialState: AnnotationWithStateType): [AnnotationWithStateType, (action: AnnotationActions, data: AnnotationWithStateType) => void] => {
-  const [state, setstate] = useState(initialState);
-
-  const update = (action: AnnotationActions, data: AnnotationWithStateType) => {
-    const state = data.state;
-    let nextState: AnnotationStates = state;
-    switch (action) {
-      case AnnotationActions.SAVE:
-        nextState = AnnotationStates.OPEN;
-        break;
-      case AnnotationActions.EDIT:
-        nextState = AnnotationStates.EDITING;
-        break;
-      case AnnotationActions.OPEN:
-        if (state !== AnnotationStates.EDITING) {
-          nextState = AnnotationStates.OPEN;
-        }
-        break;
-      case AnnotationActions.CLOSE:
-        if (state !== AnnotationStates.EDITING) {
-          nextState = AnnotationStates.CLOSED;
-        }
-        break;
-      case AnnotationActions.DRAG:
-        if (state !== AnnotationStates.EDITING) {
-          nextState = AnnotationStates.DRAGGING;
-        }
-        break;
-      case AnnotationActions.DELETE:
-        nextState = AnnotationStates.DELETING;
-        break;
-    }
-    setstate({ ...data, state: nextState });
-  }
-  return [state, update];
-}
-
 export const Annotation: FunctionComponent<IAnnotation> = ({ data, setapplicationState, save, remove }) => {
-  const [annotation, setannotation] = useAnnotation(data);
+
+  const [annotationState, setAnnnotationState] = useState(data.state);
 
   useEffect(() => {
-    if (annotation.state === AnnotationStates.EDITING) {
+    if (annotationState === AnnotationStates.EDITING) {
       setapplicationState(ApplicationState.EDIT_MODE);
     }
-  }, [annotation, setapplicationState])
+  }, [annotationState, setapplicationState])
 
-  const onMouseEnter = () => setannotation(AnnotationActions.OPEN, annotation)
-  const onMouseLeave = () => setannotation(AnnotationActions.CLOSE, annotation)
+  const onMouseEnter = () => annotationState !== AnnotationStates.EDITING && setAnnnotationState(AnnotationStates.OPEN)
+  const onMouseLeave = () => annotationState !== AnnotationStates.EDITING && setAnnnotationState(AnnotationStates.CLOSED)
 
   const onDragStartHandler = (e: React.DragEvent<HTMLDivElement>) => {
     e.persist();
+    const { pageX: x, pageY: y } = e;
     const payload = {
       annotationData: data,
-      mouseCoord: {
-        x: e.pageX,
-        y: e.pageY
-      }
+      mouseCoord: { x, y }
     }
     e.dataTransfer.setData("annotation", JSON.stringify(payload));
-    setannotation(AnnotationActions.DRAG, annotation);
+    setAnnnotationState(AnnotationStates.DRAGGING);
   }
-
-  const isDraggable = () => annotation.state !== AnnotationStates.EDITING;
 
   const onSaveHandler = async (annotation: AnnotationWithStateType) => {
     save(annotation);
-    setannotation(AnnotationActions.SAVE, annotation);
+    setAnnnotationState(AnnotationStates.OPEN);
     setapplicationState(ApplicationState.DEFAULT_MODE);
   }
 
   const onEditHandler = () => {
-    setannotation(AnnotationActions.EDIT, annotation);
+    setAnnnotationState(AnnotationStates.EDITING);
     setapplicationState(ApplicationState.EDIT_MODE);
   }
   const onDeleteHandler = () => {
-    remove(annotation);
-    setannotation(AnnotationActions.DELETE, annotation);
+    remove(data);
+    setAnnnotationState(AnnotationStates.DELETING);
     setapplicationState(ApplicationState.DEFAULT_MODE);
   }
 
   return (
     <StyledMarker
-      coord={annotation.coord}
-      annotationState={annotation.state}
+      coord={data.coord}
+      annotationState={annotationState}
       onClick={(e: React.MouseEvent) => e.stopPropagation()}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
-      draggable={isDraggable()}
+      draggable={annotationState !== AnnotationStates.EDITING}
       onDragStart={onDragStartHandler}
     >
       <AnnotationTooltip
-        data={annotation}
+        data={data}
+        annotationstate={annotationState}
         onSaveHandler={onSaveHandler}
         onEditHandler={onEditHandler}
         onDeleteHandler={onDeleteHandler}
--- src/components/AnnotationTooltip/AnnotationTooltip.tsx
+++ src/components/AnnotationTooltip/AnnotationTooltip.tsx
@@ -9,43 +9,42 @@ import { Save } from '../Icons/Save';
 import { StyledTextArea } from './StyledTextArea';
 import { PositionAnnotationTooltip } from './PositionAnnotationTooltip';
 import { AnnotationStates } from '../Annotation/Annotation';
-import { AnnotationWithStateType } from '../../types';
+import { AnnotationType } from '../../types';
 
 interface IAnnotationTooltip {
-  data: AnnotationWithStateType,
+  data: AnnotationType,
+  annotationstate: AnnotationStates,
   onSaveHandler: any;
   onEditHandler: any;
   onDeleteHandler: any;
 }
 
-export const AnnotationTooltip: FunctionComponent<IAnnotationTooltip> = ({ data, onSaveHandler
-  , onEditHandler
-  , onDeleteHandler }) => {
+export const AnnotationTooltip: FunctionComponent<IAnnotationTooltip> = ({ data, annotationstate, onSaveHandler, onEditHandler, onDeleteHandler }) => {
 
   const textAreaRef = useRef<HTMLTextAreaElement>({} as HTMLTextAreaElement);
 
   const [annotatedText, setIsAnnotatedText] = useState(data.text);
 
   useEffect(() => {
-    if (data.state === AnnotationStates.EDITING) {
+    if (annotationstate === AnnotationStates.EDITING) {
       textAreaRef?.current?.focus();
     }
-  }, [data])
+  }, [annotationstate])
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) => setIsAnnotatedText(e.target.value);
 
   return (
-    <PositionAnnotationTooltip annotationState={data.state}>
-      <StyledAnnotationTooltip annotationState={data.state}>
+    <PositionAnnotationTooltip annotationState={annotationstate}>
+      <StyledAnnotationTooltip annotationState={annotationstate}>
         <StyledTextArea
           ref={textAreaRef}
           value={annotatedText}
-          inEditMode={data.state === AnnotationStates.EDITING}
+          inEditMode={annotationstate === AnnotationStates.EDITING}
           onChange={onChangeHandler}
         />
         <StyledBtnWrapper>
           {
-            data.state === AnnotationStates.EDITING ?
+            annotationstate === AnnotationStates.EDITING ?
               <Save onClickHandler={() => onSaveHandler({ ...data, text: annotatedText })} /> : <Pencil onClickHandler={onEditHandler} />
           }
           <Bin onClickHandler={onDeleteHandler} />
